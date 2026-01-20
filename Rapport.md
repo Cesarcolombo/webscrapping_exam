891bad8499e0d5fa9065e69ba0f5982b86c1e2b0
Cesar Colombo <cesarcolombo3@gmail.com>
2026-01-20 17:01:47 +0100

    fin du premier cours

---
+++ Rapport.md
@@ -0,0 +1,3 @@
+# webscrapping_exam Rapport
+
+On séppart le travail en deux partie. Le web scapping et la database. et on mergera par la suite les différents fichiers.
---
+++ webscrapping.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+
+#url = "https://quotes.toscrape.com/page/1/"
+#response = requests.get(url)
+#html = response.text
+
+#if response.status_code != 200:
+ #   print("erreur: ", response.status_code)
+
+
+#print(str(type(html)))
+#soup = BeautifulSoup(html, "html.parser")
+#print(str(type(soup)))
+
+
+#print(soup)
+def creer_soup(url):
+    response = requests.get(url)
+    html = response.text
+
+    if response.status_code != 200:
+        print("erreur: ", response.status_code)
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+
+
+def extraire_sur_une_page(soup):
+    quotes = soup.find_all("div", class_="quote")
+    for quote in quotes:
+        citation = quote.find("span", class_="text").get_text()
+        tags = [tag.get_text() for tag in quote.find_all("a", class_="tag")]
+        return [(citation[k] , tags[k]) for k in range(len(citation))]
+#extraire_sur_une_page(soup)
+#print(str(extraire_sur_une_page(soup)))
+
+
+def extraire_sur_plusieurs_pages(soup):
+    donnee = []
+    k = 1
+    while k < 30:
+        url = f"https://quotes.toscrape.com/page/{k}/"
+        soup = creer_soup(url)
+        donnee += extraire_sur_une_page(soup)
+        k += 1
+    return donnee
+
+
+
+
+
+
+def sauvegarder_donnees(donnees):
+    pass
+
