c5122ec386581281980d4a4f132b7ddf0965148a
Alexandre Rinchet <alexandre.rinchet@etu.minesparis.psl.eu>
2026-01-20 17:02:15 +0100

    classe databasemanager : cree_table et init

--- database.py
+++ database.py
@@ -1,4 +1,33 @@
 import sqlite3
 
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
+
+class DatabaseManager :
+    def __init__(self, db_name = "database.db") :
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.cree_table()
+
+    def cree_table(self) :
+        query = """
+        CREATE TABLE IF NOT EXISTS annonces (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titre TEXT NOT NULL,
+            prix INTEGER NOT NULL,
+            surface INTEGER,
+            ville TEXT NOT NULL,
+            nb_pieces INTEGER,
+            url TEXT UNIQUE,
+            date_scraping TEXT NOT NULL
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def insere_annonce() :
+        pass
+
+    def lit_donnees() :
+        pass
+
+    def ferme_base() :
+        pass
