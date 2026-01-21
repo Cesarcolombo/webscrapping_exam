0ecd781236b1c516755274d6468804a9986b5c1f
Cesar Colombo <cesarcolombo3@gmail.com>
2026-01-21 14:16:17 +0100

    webscrapping fini et test sur data

--- webscrapping.py
+++ webscrapping.py
@@ -28,18 +28,20 @@ def creer_soup(url):
 
 def extraire_sur_une_page(soup):
     quotes = soup.find_all("div", class_="quote")
+    tags = []
+    citation = []
     for quote in quotes:
-        citation = quote.find("span", class_="text").get_text()
-        tags = [tag.get_text() for tag in quote.find_all("a", class_="tag")]
-        return [(citation[k] , tags[k]) for k in range(len(citation))]
+        citation .append(quote.find("span", class_="text").get_text())
+        tags.append([tag.get_text() for tag in quote.find_all("a", class_="tag")])
+    return [(citation[k] , tags[k]) for k in range(len(citation))]
 #extraire_sur_une_page(soup)
 #print(str(extraire_sur_une_page(soup)))
 
 
-def extraire_sur_plusieurs_pages(soup):
+def extraire_sur_plusieurs_pages(max):
     donnee = []
     k = 1
-    while k < 30:
+    while k < max + 1:
         url = f"https://quotes.toscrape.com/page/{k}/"
         soup = creer_soup(url)
         donnee += extraire_sur_une_page(soup)
@@ -48,6 +50,13 @@ def extraire_sur_plusieurs_pages(soup):
 
 
 
+
+donnee = extraire_sur_plusieurs_pages(1)
+
+print(donnee[:2])  
+import json
+with open("data.json", "w", encoding="utf-8") as f:
+    json.dump(donnee, f, ensure_ascii=False, indent=4)
 
 
 
